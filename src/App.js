222c14800808a880d5f16d02205c4f48940a3599
hoangminh24111998 <t.hoangminh2411@gmail.com>
2021-08-05 23:48:27 +0700

    Buoi4-OnTapSateProps

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import FunctionComponent from "./Components/FunctionComponent";
+// import ClassComponent from "./Components/ClassComponent";
+// import HomeComponent from "./Components/BaiTapLayoutComponent/HomeComponent";
+// import Databinding from "./Databinding/Databinding"
+// import BaiTapThucHanhLayout from "./Components/BaiTapThucHanhLayout/BaiTapThucHanhLayout";
+// import Demo from "./Databinding/Demo";
+// import HandleEvent from "./HandleEvent/HandleEvent";
+// import StyleComponent from "./StyleComponent/StyleComponent";
+// import StateDemo from "./StateDemo/StateDemo"
+// import HomeLayout from "./Props/DemoProps/HomeLayout";
+// import RenderWithMap from "./RenderWithMap/RenderWithMap";
+// import ShoesShop from "./Props/ShoesShop/ShoesShop";
+// import BaiTapXemChiTiet from "./Props/BaiTapXemChiTiet/BaiTapXemChiTiet";
+import ExerciseCarStore from "./Props/ExerciseCarStore/ExerciseCarStore";
+
+function App() {
+  return (
+    <div className="App">
+          {/* <HomeComponent /> */}
+          {/* <Databinding /> */}
+          {/* <BaiTapThucHanhLayout /> */}
+          {/* <Demo />  */}
+        
+          {/* <HandleEvent></HandleEvent> */}
+
+          {/* <StyleComponent /> */}
+          {/* <p className="fontBold">ahihi</p> */}
+          {/* <StateDemo /> */}
+          {/* <HomeLayout /> */}
+          {/* <RenderWithMap /> */}
+          {/* <ShoesShop /> */}
+          {/* <BaiTapXemChiTiet /> */}
+          <ExerciseCarStore />
+    </div>
+  );
+}
+
+export default App;
+
+
